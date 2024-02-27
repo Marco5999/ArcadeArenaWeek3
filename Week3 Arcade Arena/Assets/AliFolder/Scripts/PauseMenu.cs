6cdcfde03ca9248c6cf6462f6710fb5b145cb2fb
Aly Sharaf <ali.charaf59@gmail.com>
2024-02-27 16:32:28 +0400

    Completed Hub, Created Start menu, and in game menu (one for hub and one for the levels)

---
+++ Week3 Arcade Arena/Assets/AliFolder/Scripts/PauseMenu.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public bool isPaused;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToHub()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("HubScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
