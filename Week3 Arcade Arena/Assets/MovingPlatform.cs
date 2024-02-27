5449dd3cffc4791da73c2f740d34aab4f68097e6
Mohammad Thani <mohammad.almheiri@student.saedubai.com>
2024-02-27 17:21:02 +0400

    Moving Platform Fix

--- Week3 Arcade Arena/Assets/MovingPlatform.cs
+++ Week3 Arcade Arena/Assets/MovingPlatform.cs
@@ -4,43 +4,25 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    public List<Transform> points;
+    public int currentPoint;
+
     public float speed;
     private float movement;
-
-    public GameObject PositionA;
-    public GameObject PositionB;
-    public bool movingToA = true;
-    public bool movingToB;
 
     void FixedUpdate()
     {
         movement = speed * Time.fixedDeltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, points[currentPoint].position, movement);
 
-        if (movingToA)
+        if (transform.position == points[currentPoint].position)
         {
-            transform.position = Vector3.MoveTowards(transform.position, PositionA.transform.position, movement);
-        }
-
-        if (movingToB)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, PositionB.transform.position, movement);
-        }
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "PositionA")
-        {
-            Debug.Log("Changing to B");
-            movingToA = false;
-            movingToB = true;
-        }
-
-        else if (other.gameObject.tag == "PositionB")
-        {
-            Debug.Log("Changing to A");
-            movingToA = true;
-            movingToB = false;
+            if (currentPoint == points.Count - 1)
+            {
+                currentPoint = 0;
+            } else {
+                currentPoint += 1;
+            }
         }
     }
 }
